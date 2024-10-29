f5f49ba4066c89785cdfeea277ea5f2160b39d10
JDrost04 <533567@student.fontys.nl>
2024-10-29 19:26:39 +0100

    nog eentje

--- how_it_works.py
+++ how_it_works.py
@@ -1,12 +1,9 @@
 import streamlit as st
 
 st.title("How It Works")
-st.write("This is how the **Bus Planning Checker** works.")
 
-# Gebruik Markdown voor een nettere opsomming
 st.write("The app checks the following conditions:")
 
-# Maak gebruik van een genummerde lijst met Markdown
 st.markdown("""
 1. **Battery Status**: Ensures the battery status is not under **10%** of the State of Health, which is **30 kWh**.
 2. **Route Endpoint**: Verifies if the endpoint of route **n** matches the start point of the next route.
